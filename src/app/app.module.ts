190107361f6f37269102a05e507275b0bfa69b53
Patrik Eriksson <patrikeriksson@Patriks-MacBook-Pro-2.local>
2021-05-05 21:02:59 +0200

    Add home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NavigationModule } from 'src/components/navigation/navigation.module'
+import { HomeComponent } from 'src/pages/home/home.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,9 +10,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NavigationModule,
   ],
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   providers: [],
   bootstrap: [
---
+++ src/interfaces/ApiResponse.ts
@@ -0,0 +1,7 @@
+export default interface ApiResponse {
+  id: string;
+  accountId: string;
+  bank: string;
+  balance: number;
+  currency: string;
+}
